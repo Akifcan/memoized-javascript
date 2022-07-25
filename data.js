0d38b62f99ade4a5f6a35c7cf49aa25bc4d1eb37
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-07-25 10:43:26 +0300

    initial

---
+++ data.js
@@ -0,0 +1,77 @@
+const hotels = [
+    {
+        name: 'Bornova Hotel',
+        city: 'İzmir',
+        peopleCount: 2,
+        pricePerPerson: 900,
+        currency: '₺',
+        features: ['tv', 'internet', 'buzdolabı', 'kahvaltı', 'akşam yemeği'],
+        thumbnail: 'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG90ZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        name: 'Urla Hotel',
+        city: 'İzmir',
+        peopleCount: 4,
+        features: ['tv', 'internet', 'buzdolabı', 'kahvaltı'],
+        pricePerPerson: 400,
+        currency: '₺',
+        thumbnail: 'https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        name: 'Marmara Hotel',
+        city: 'İstanbul',
+        peopleCount: 2,
+        pricePerPerson: 200,
+        features: ['tv', 'internet', 'buzdolabı'],
+        currency: '₺',
+        thumbnail: 'https://images.unsplash.com/photo-1444201983204-c43cbd584d93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        name: 'İstanbul Otel',
+        city: 'İstanbul',
+        peopleCount: 4,
+        features: ['tv', 'internet', 'buzdolabı', 'kahvaltı', 'akşam yemeği'],
+        pricePerPerson: 900,
+        currency: '₺',
+        thumbnail: 'https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        name: 'Deniz Kenarı Otel',
+        city: 'Muğla',
+        peopleCount: 2,
+        pricePerPerson: 1500,
+        features: ['tv', 'internet', 'buzdolabı', 'açık büfe', 'akşam yemeği', 'havuz', 'sahil'],
+        currency: '₺',
+        thumbnail: 'https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+    },
+    {
+        name: 'Seferihisar otel',
+        city: 'İzmir',
+        peopleCount: 2,
+        pricePerPerson: 1500,
+        features: ['tv', 'internet', 'buzdolabı', 'açık büfe', 'akşam yemeği', 'havuz', 'sahil'],
+        currency: '₺',
+        thumbnail: 'https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+    },
+    {
+        name: 'Mavi Renk Otel',
+        city: 'Muğla',
+        peopleCount: 4,
+        features: [''],
+        pricePerPerson: 300,
+        features: ['tv', 'internet', 'buzdolabı'],
+        currency: '₺',
+        thumbnail: 'https://images.unsplash.com/photo-1586611292717-f828b167408c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+    },
+    {
+        name: 'Fethiye Otel',
+        city: 'Muğla',
+        peopleCount: 2,
+        features: ['tv', 'internet', 'buzdolabı'],
+        pricePerPerson: 500,
+        currency: '₺',
+        thumbnail: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+]
+
+export default hotels
---
+++ script.js
@@ -0,0 +1,69 @@
+import hotels from './data.js'
+const searchForm = document.querySelector('.search')
+const featuresBox = searchForm.querySelector('#features')
+const main = document.querySelector('main')
+
+function setSelectbox(data, parent) {
+    data.forEach(x => {
+        const option = document.createElement('option')
+        option.setAttribute('value', x)
+        option.textContent = x
+        parent.appendChild(option)
+    })
+}
+
+function getResult(personCount, city, features) {
+    console.log(personCount, city, features)
+    return new Promise((resolve, _) => {
+        let result
+        const args = []
+        main.innerHTML = 'Lütfen bekleyin'
+        setTimeout(() => {
+            if (personCount) {
+                result = (result || hotels).filter(hotel => hotel.peopleCount === +personCount)
+                args.push(personCount)
+            }
+
+            if (city) {
+                result = (result || hotels).filter(hotel => hotel.city === city)
+                args.push(city)
+            }
+
+            if (features.length) {
+                result = (result || hotels).filter(hotel => hotel.features.join(',').includes(features))
+                args.push(features)
+            }
+            resolve(result)
+        }, 5000)
+    })
+}
+
+function listResults(data) {
+    main.innerHTML = data.map(hotel => {
+        return `
+            <div class="card">
+                <picture>
+                    <img src="${hotel.thumbnail}"
+                        alt="${hotel.name}">
+                </picture>
+                <h2>${hotel.name} - <b>${hotel.city}</b></h2>
+                <p style="text-transform: capitalize;">${hotel.features.join(', ')}</p>
+                <p>${hotel.pricePerPerson * hotel.peopleCount}${hotel.currency}</p>
+            </div>
+
+        `
+    }).join('')
+
+}
+
+setSelectbox([...new Set(hotels.map(hotel => hotel.city))], searchForm.querySelector('#cities'))
+setSelectbox([...new Set(hotels.map(hotel => hotel.features).flat(1))], featuresBox)
+listResults(hotels)
+
+searchForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const formData = new FormData(searchForm)
+    const features = [...featuresBox.selectedOptions].map(x => x.value)
+    const { personCount, city } = Object.fromEntries(formData)
+    listResults(await getResult(personCount, city, features))
+})
